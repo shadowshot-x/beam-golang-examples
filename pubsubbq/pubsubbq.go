e453ea574a3a661530584cbacc92430481a9a6c6
Ujjwal Sharma <ujjwal26599@gmail.com>
2023-04-11 15:27:47 +0530

    naming correction

--- pubsubbq/pubsubbq.go
+++ pubsubbq/pubsubbq.go
@@ -3,6 +3,8 @@ package pubsubbq
 import (
 	"context"
 	"flag"
+	"fmt"
+	"strconv"
 	"strings"
 	"time"
 
@@ -21,11 +23,18 @@ import (
 // 3. Dataflow Admin and Dataflow Worker permission should be assigned to SA.
 // 4. BigQuery dataset and table must be created.
 
+// Use Case:-
+// 1. Using Golang Apache Beam SDK with Dataflow, Pubsub, BigQuery
+// 2. We are getting sensor data from IoT devices to PubSub in the form {device_num}:{cluster_num}-{value_num}
+// 3. We need to analyze the data for only 2 cluster numbers.
+// 4. For cluster1, values are in per minute. These need to be converted to per second ie. multiply by 60.
+// 5. We need to add these to BQ Table grouped by device number with values separated by comma along with append time. This needs to be done in time window of 10 seconds.
+
 type (
 	BQRow struct {
-		Table  string
-		Datum1 string
-		Datum2 string
+		Device string
+		Values string
+		Time   string
 	}
 )
 
@@ -38,75 +47,82 @@ func PubsubToBigQuery() {
 	p := beam.NewPipeline()
 	s := p.Root()
 
-	pCollection0 := pubsubio.Read(s, "sharmaujjwal-sce", "pubsub-test", &pubsubio.ReadOptions{
+	pubsub_messages := pubsubio.Read(s, "sharmaujjwal-sce", "pubsub-test", &pubsubio.ReadOptions{
 		Subscription: "pubsub-test-sub",
 	})
 
 	// Data Enrichment by appending the existing date to the pubsub message
 	// Data Filtering by sending data to different pardo based on data values.
-	pCollection1, pCollection2 := beam.ParDo2(s, func(b []byte, pCollection1, pCollection2 func(string)) {
+	messages_cluster1, messages_cluster2 := beam.ParDo2(s, func(b []byte, pCollection1, pCollection2 func(string)) {
 		msg := string(b)
-		if strings.Contains(msg, "data1") {
-			pCollection1(msg + "-" + time.Now().String())
-		} else if strings.Contains(msg, "data2") {
-			pCollection2(msg + "-" + time.Now().String())
+		if strings.Contains(msg, "cluster1") {
+			pCollection1(msg)
+		} else if strings.Contains(msg, "cluster2") {
+			pCollection2(msg)
 		}
-	}, pCollection0)
+	}, pubsub_messages)
 
-	// Data Cleaning and Correction
-	pCollection_datum_1 := beam.ParDo(s, func(ss string) (string, string) {
+	// Data Correction
+	cluster1_kv_pair := beam.ParDo(s, func(ss string) (string, string) {
 		z := strings.Split(ss, ":")
 		if len(z) == 1 {
-			return "table1", z[0]
+			return "device1", z[0]
 		}
-		return z[0], strings.Replace(z[1], "ta", "tum", 1)
-	}, pCollection1)
+		zz := strings.Split(ss, "-")[1]
 
-	pCollection_datum_2 := beam.ParDo(s, func(ss string) (string, string) {
+		value, err := strconv.Atoi(zz)
+		if err != nil {
+
+		}
+		value = value * 60
+		return z[0], strings.Split(ss, "-")[1] + "-" + fmt.Sprintf("%v", value)
+	}, messages_cluster1)
+
+	cluster2_kv_pair := beam.ParDo(s, func(ss string) (string, string) {
 		z := strings.Split(ss, ":")
 		if len(z) == 1 {
-			return "table2", z[0]
+			return "device2", z[0]
 		}
 		return z[0], z[1]
-	}, pCollection2)
+	}, messages_cluster2)
 
 	// Windowing of 10 seconds added.
-	windowedLines_tableName := beam.WindowInto(s, window.NewFixedWindows(time.Second*10), pCollection_datum_1)
-	windowedLines_data := beam.WindowInto(s, window.NewFixedWindows(time.Second*10), pCollection_datum_2)
+	windowed_cluster1 := beam.WindowInto(s, window.NewFixedWindows(time.Second*10), cluster1_kv_pair)
+	windowed_cluster2 := beam.WindowInto(s, window.NewFixedWindows(time.Second*10), cluster2_kv_pair)
 
 	// Group by using keys and values
-	pCollection_GBK := beam.CoGroupByKey(s, windowedLines_tableName, windowedLines_data)
+	pCollection_GBK := beam.CoGroupByKey(s, windowed_cluster1, windowed_cluster2)
 
 	// Run the aggregation based on keys for the windowed input
 	// This will run every 10 seconds
-	pCollection_key_aggregation := beam.ParDo(s, func(key string, value1, value2 func(*string) bool) string {
+	key_aggregation := beam.ParDo(s, func(key string, value1, value2 func(*string) bool) string {
 		var s_z string
 		var v1_list, v2_list string
 		for value1(&s_z) {
-			v1_list = v1_list + " , " + s_z
+			v1_list = v1_list + " , " + strings.Split(s_z, "-")[1]
 		}
 		for value2(&s_z) {
-			v2_list = v2_list + " , " + s_z
+			v2_list = v2_list + " , " + strings.Split(s_z, "-")[1]
 		}
-		return key + " :::: " + v1_list + " :::: " + v2_list
+		return key + " :::: " + v1_list + v2_list
 
 	}, pCollection_GBK)
 
 	// Create the BQ Row PCollection by processing the output
-	pCollection_BQRow := beam.ParDo(s, func(ss string) BQRow {
+	create_BQRow := beam.ParDo(s, func(ss string) BQRow {
 		z := strings.Split(ss, "::::")
 		return BQRow{
-			Table:  z[0],
-			Datum1: z[1],
-			Datum2: z[2],
+			Device: z[0],
+			Values: z[1],
+			Time:   time.Now().String(),
 		}
-	}, pCollection_key_aggregation)
+	}, key_aggregation)
 
 	// print the output
-	debug.Print(s, pCollection_key_aggregation)
+	debug.Print(s, key_aggregation)
 
 	// write to bigquery
-	bigqueryio.Write(s, "sharmaujjwal-sce", "sharmaujjwal-sce:test_dataset.test_table", pCollection_BQRow)
+	bigqueryio.Write(s, "sharmaujjwal-sce", "sharmaujjwal-sce:test_dataset.test_table", create_BQRow)
 
 	// Run the beam pipeline
 	if err := beamx.Run(ctx, p); err != nil {
